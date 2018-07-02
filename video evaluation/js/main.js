b470e5ff25565cd785ca49bd759e530a1969103a
Albert <Albertios@users.noreply.github.com>
2018-07-02 22:58:08 +0200

    Video Evaluation website
    a tool to get the current time from the video and save the time in json

---
+++ video evaluation/js/main.js
@@ -0,0 +1,62 @@
+
+
+var vid = document.getElementById("myVideo");
+
+
+function getCurCarTime() {
+    trafficList.cars.push({ carTime: vid.currentTime });
+};
+
+function getCurBicycleTime() {
+    trafficList.bicycles.push({ bicycleTime: vid.currentTime });
+};
+
+function getCurTruckTime() {
+    trafficList.trucks.push({ truckTime: vid.currentTime })
+};
+
+
+//data
+var trafficList = new Vue({
+    el: '#trafficList',
+    data: {
+        cars: [],
+        bicycles: [],
+        trucks: [],
+    }
+});
+
+//Keyboard Shortcut
+Mousetrap.bind('1', function getCurCarTime() {
+    trafficList.cars.push({ carTime: vid.currentTime });
+
+});
+//Keyboard Shortcut
+Mousetrap.bind('2', function getCurBicycleTime() {
+    trafficList.bicycles.push({ bicycleTime: vid.currentTime });
+});
+//Keyboard Shortcut
+Mousetrap.bind('3', function getCurTruckTime() {
+    trafficList.trucks.push({ truckTime: vid.currentTime });
+});
+
+$(document).ready(function(){
+    $("#myVideo").on(
+        "timeupdate",
+        function(event){
+            onTrackedVideoFrame(this.currentTime, this.duration);
+        });
+});
+
+//show current && duration
+function onTrackedVideoFrame(currentTime, duration){
+    $("#current").text(currentTime); //Change #current to currentTime
+    $("#duration").text(duration)
+}
+
+//can't close window be mistake
+window.onbeforeunload = function() {
+    return "Bye now!";
+};
+
+
