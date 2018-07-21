471f8b30a973eeaed3a1d0ccf6c4c6a12948692b
Albert <Albertios@users.noreply.github.com>
2018-07-21 16:07:05 +0200

    new directory/ path

--- R/scripts/Heisenbergstrasse.R
+++ R/scripts/Heisenbergstrasse.R
@@ -1,30 +1,43 @@
-library(ggplot2) 
+library(ggplot2)        # plot
 library(lubridate)      # easily work with dates and times
 library(zoo)            # working with time series data
+library(jsonlite)       # json
 #___________________________________________________________________________________________________________________________
 
 # main folder
-setwd("/Users/Alf/Documents/GitHub/Bachelor/R/scripts/")
+setwd("/Users/Alf/Documents/GitHub/Bachelor/R/")
 # load data
 
 #___________________________________________________________________________________________________________________________
- datei1test <- test123("./4_July_Heisenbergstrasse.csv", 1) 
+# datei1test <- test123("./4_July_Heisenbergstrasse.csv", 1) 
 #___________________________________________________________________________________________________________________________
 
 
-test123 <- function(CSVdata, vehicleFilter){
+#test123 <- function(CSVdata, vehicleFilter){
 
-dataFromHeisenbergstrasse <- read.table(file = CSVdata, sep = ",")
+#dataFromHeisenbergstrasse <- read.table(file = CSVdata, sep = ",")
 
-#change name of the first column
+dataFromHeisenbergstrasse <- read.table(file = "./csv/4_July_Heisenbergstrasse.csv", sep = ",")
+
+#change head name of the first & second column
 colnames(dataFromHeisenbergstrasse) <- c("timeInMilliseconds", "signalStrength")
 
+jsonCar <- fromJSON("./json/carTime4th7.json")
+
+# minus video time until adruino turns on; multiply 1000 to convert seconds to millisecond
+jsonCar <- (jsonCar - 589.726292) * 1000
+
+# Remove elements larger than max of timeInMilliseconds:
+jsonCar <- jsonCar[ jsonCar < max(dataFromHeisenbergstrasse$timeInMilliseconds) ]
+
+
 # convert in minutes
-timeInMinutes <- format( as.POSIXct(Sys.Date())+dataFromHeisenbergstrasse$timeInMilliseconds/1000, "%M:%S")
+#timeInMinutes <- format( as.POSIXct(Sys.Date())+dataFromHeisenbergstrasse$timeInMilliseconds/1000, "%M:%S")
 
 #plot
 print(ggplot(dataFromHeisenbergstrasse, aes(x=timeInMilliseconds, y=signalStrength)) + geom_line() +
-  labs(title="Heisenbergstrasse 4/7/2018 WI-Fi Strengths:", x = "Time", y = "Strength")) 
+        geom_vline(xintercept = jsonCar, colour="red", linetype = 3) +
+        labs(title="Heisenbergstrasse 4/7/2018 WI-Fi Strengths:", x = "Time", y = "Strength")) 
 #___________________________________________________________________________________________________________________________
 
 #get data where strength is under -65
@@ -32,7 +45,8 @@ subsetDataFromHeisenbergstrasseGreaterThanMinus65 <- dataFromHeisenbergstrasse[w
 
 #plot
 print(ggplot(subsetDataFromHeisenbergstrasseGreaterThanMinus65, aes(x=timeInMilliseconds, y=signalStrength)) + geom_line() +
-  labs(title="Heisenbergstrasse 4/7/2018 WI-Fi Strengths:", x = "Time", y = "Strength") )
+        geom_vline(xintercept = jsonCar, colour="red", linetype = 3) +
+        labs(title="Heisenbergstrasse 4/7/2018 WI-Fi Strengths:", x = "Time", y = "Strength") )
 
 #___________________________________________________________________________________________________________________________
 
@@ -46,7 +60,7 @@ if (length(amplitudeDataFromHeisenbergstrasse$signalStrength)%%2 != 0 ){
   amplitudeDataFromHeisenbergstrasseLength = length(amplitudeDataFromHeisenbergstrasse$signalStrength)
 }
 
-listV3 <- amplitudeDataFromHeisenbergstrasse$V3
+AmplitudeList <- amplitudeDataFromHeisenbergstrasse$V3
 listSignalStrength <- amplitudeDataFromHeisenbergstrasse$signalStrength
 
 #first measurement - second measurement
@@ -54,7 +68,7 @@ for (i in 1:(amplitudeDataFromHeisenbergstrasseLength)){
   
   nextValue <-listSignalStrength[i+1]
   
-  listV3[i]<- c(if (listSignalStrength[i] <= nextValue) {
+  AmplitudeList[i]<- c(if (listSignalStrength[i] <= nextValue) {
     
     0
     
@@ -65,20 +79,24 @@ for (i in 1:(amplitudeDataFromHeisenbergstrasseLength)){
   
 }
 
-listV4 <- amplitudeDataFromHeisenbergstrasse$V4
+filteredAmplitudeList <- NA
+
+vehicleFilter = 5
 
 for (i in 1: (amplitudeDataFromHeisenbergstrasseLength)){
-  listV4[i]<- c(if (listV3[i] < vehicleFilter) {
+  filteredAmplitudeList[i]<- c(if (AmplitudeList[i] <= vehicleFilter) {
     0
   }else{
-    listV3[i]
+    AmplitudeList[i]
   })
 }
-return(listV4)
+return(filteredAmplitudeList)
 
-print(ggplot(amplitudeDataFromHeisenbergstrasse ,aes(x=timeInMilliseconds, y=listV4 )) + geom_line() + 
-  labs(title="amplitude:", x = "time", y = "Strength:")  )
-} # function end
+#plot
+print(ggplot(data=data.frame(x=dataFromHeisenbergstrasse$timeInMilliseconds[-length(dataFromHeisenbergstrasse$timeInMilliseconds) ] , y=filteredAmplitudeList) ,aes(x=x, y=y )) + geom_line() + 
+        geom_vline(xintercept = jsonCar, colour="red", linetype = 3) +
+        labs(title="amplitude:", x = "time", y = "Strength:")  )
+#} # function end
 
 #___________________________________________________________________________________________________________________________
 
@@ -86,15 +104,15 @@ print(ggplot(amplitudeDataFromHeisenbergstrasse ,aes(x=timeInMilliseconds, y=lis
 
 #the value must be greater than x otherwise 0
 # amplitudeFuction <- function(vehicleFilter, amplitudeDataFromHeisenbergstrasseLength) {
-#   listV4
+#   filteredAmplitudeList
 #   for (i in 1: (amplitudeDataFromHeisenbergstrasseLength+1)){
-#     listV4[i]<- c(if (listV3[i] < vehicleFilter) {
+#     filteredAmplitudeList[i]<- c(if (AmplitudeList[i] < vehicleFilter) {
 #       0
 #     }else{
-#       listV3[i]
+#       AmplitudeList[i]
 #     })
 #   }
-#   return(listV4)
+#   return(filteredAmplitudeList)
 # }
 
 
@@ -111,13 +129,7 @@ print(ggplot(amplitudeDataFromHeisenbergstrasse ,aes(x=timeInMilliseconds, y=lis
 
 
 
-
-
-
-
-
-
-
+sum(filteredAmplitudeList > 5 )
 
 
 
